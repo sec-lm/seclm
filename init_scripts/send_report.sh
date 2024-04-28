5c58ef6ee888bde8c3c3d5a4c7867b83fbfc6db4
stneng <git@stneng.com>
2024-04-27 21:14:05 -0700

    fix

--- init_scripts/send_report.sh
+++ init_scripts/send_report.sh
@@ -5,6 +5,6 @@ export SECLM_TMP_PORT=80
 
 sudo setcap 'cap_net_bind_service=+ep' `which python3.10`
 
-curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/sec-lm/snp-tools/42b013e1ab378f0e332a20fd69498c28f24a5982/guest_tools/send_report.py | python3
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/sec-lm/snp-tools/72fd7d38ff2b5afe89730ae207f5aad1e2935445/guest_tools/send_report.py | python3
 
 unset SECLM_TMP_PORT
