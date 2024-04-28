72fd7d38ff2b5afe89730ae207f5aad1e2935445
stneng <git@stneng.com>
2024-04-27 21:13:40 -0700

    fix

--- guest_tools/send_report.py
+++ guest_tools/send_report.py
@@ -1,10 +1,22 @@
 import socket
 import os
+import time
 
 REPORT_FILE = os.path.expanduser("~/report.bin")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("0.0.0.0", int(os.environ["SECLM_TMP_PORT"])))
+count = 0
+while True:
+    try:
+        s.bind(("0.0.0.0", int(os.environ["SECLM_TMP_PORT"])))
+        break
+    except:
+        time.sleep(5)
+        print("Waiting...")
+        count += 1
+        if count > 12:
+            raise
+
 s.listen(1)
 
 conn, addr = s.accept()
